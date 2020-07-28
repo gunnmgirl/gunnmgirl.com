fd491537bbe01e0d147365779618ad537bcf2216
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2020-07-28 12:08:32 +0200

    Adds grid

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+
+import Header from "./features/Header";
+import Content from "./features/Content";
+import SocialMedia from "./features/SocialMedia";
+import GlobalStyle from "./GlobalStyle";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 1fr 2fr 0.6fr;
+  min-height: 100vh;
+  height: 100%;
+`;
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Container>
+      <GlobalStyle />
+      <Header />
+      <Content />
+      <SocialMedia />
+    </Container>
+  );
 }
 
 export default App;
