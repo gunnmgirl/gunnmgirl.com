91e5ba094ccddde21e35f7edf2e662325e4931f1
gunnmgirl <gunnmgirl@gmail.com>
2024-07-19 18:02:12 +0200

    feat: adds framer motion list animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import { useState } from "react";
 import { SocialMediaBox } from "./components/SocialMediaBox";
 import { Projects } from "./components/Projects";
 import { projects } from "./static/projects";
@@ -7,8 +8,6 @@ import profilePic from "./static/images/dzana.jpg";
 import { Github } from "./icons/Github";
 import { Linkedin } from "./icons/Linkedin";
 import { Twitter } from "./icons/Twitter";
-import { useState } from "react";
-import clsx from "clsx";
 
 export default function Home() {
   const projectsGridOne = projects.slice(0, 4);
@@ -21,21 +20,16 @@ export default function Home() {
 
   return (
     <div className="grid grid-cols-[1fr_2fr_1fr] gap-2 h-lvh overflow-hidden">
-      <div>{/* {view && <Projects projects={projectsGridOne} />} */}</div>
-      <div
-        className={clsx(
-          "h-screen flex flex-col items-center justify-evenly p-4 md:p-0 transition-all duration-500",
-          view ? "mx-16" : "m-0"
-        )}
-      >
-        {/* <div>
+      <div>{view && <Projects projects={projectsGridOne} />}</div>
+      <div className="h-screen flex flex-col items-center justify-evenly p-4 md:p-0 transition-all duration-7000">
+        <div>
           <button
             onClick={handleView}
             className="bg-gradient-to-l from-[#FF69B4] to-[#FF69B4]/60 hover:from-[#BA55D3] hover:to-[#BA55D3]/60 hover:scale-105 font-bold text-white p-2 rounded-md transition-all duration-300"
           >
             {view ? "Hide Projects" : "View Projects"}
           </button>
-        </div> */}
+        </div>
         <div className="flex flex-col items-center gap-6 sm:px-[8%] md:px-[15%] lg:px-[20%]">
           <Image
             src={profilePic}
@@ -80,7 +74,7 @@ export default function Home() {
           <SocialMediaBox icon={<Twitter />} href="https://x.com/gunnmgirl98" />
         </div>
       </div>
-      <div>{/* {view && <Projects projects={projectsGridTwo} />} */}</div>
+      <div>{view && <Projects projects={projectsGridTwo} isOdd />}</div>
     </div>
   );
 }
