ca0d4f2f1b77a60ee29bef30c00b0a95743a7b99
gunnmgirl <gunnmgirl@gmail.com>
2024-08-01 16:06:19 +0200

    feat: updates project box padding on scale effect

--- app/components/ProjectBox.tsx
+++ app/components/ProjectBox.tsx
@@ -10,16 +10,18 @@ export const ProjectBox = (props: ProjectBoxProps) => {
   };
 
   return (
-    <div className="hover:scale-110 transition-all duration-400 max-w-sm p-3 flex flex-col gap-1 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <Image src={projectImage} alt="Picture of the project" />
-      <h1 className="font-extrabold">{title}</h1>
-      <p className="text-sm">{description}</p>
-      <button
-        onClick={handleViewProject}
-        className="mt-2 text-sm w-max bg-gradient-to-l from-[#FF69B4] to-[#FF69B4]/60 hover:scale-105 font-bold text-white p-1 rounded-md transition-all duration-300"
-      >
-        Go to website
-      </button>
+    <div className="m-3 p-3 transition-all duration-400 hover:scale-110">
+      <div className="max-w-sm flex flex-col gap-1 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-3">
+        <Image src={projectImage} alt="Picture of the project" />
+        <h1 className="font-extrabold">{title}</h1>
+        <p className="text-sm">{description}</p>
+        <button
+          onClick={handleViewProject}
+          className="mt-2 text-sm w-max bg-gradient-to-l from-[#FF69B4] to-[#FF69B4]/60 hover:scale-105 font-bold text-white p-1 rounded-md transition-all duration-300"
+        >
+          Go to website
+        </button>
+      </div>
     </div>
   );
 };
