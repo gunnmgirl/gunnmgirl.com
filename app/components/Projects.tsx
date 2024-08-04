4fb52f515793ac3eec9adaa34259aabdb09aacd1
gunnmgirl <gunnmgirl@gmail.com>
2024-08-04 20:29:46 +0200

    feat: pause animation on child element hover

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -10,22 +10,33 @@ export const Projects = ({
   projects: ProjectBoxProps[];
   isOdd?: boolean;
 }) => {
+  // pause animation on child element hover
+  const motionProps = {
+    y: isOdd ? ["-100%", "100%"] : ["100%", "-100%"],
+    transition: {
+      y: {
+        repeat: Infinity,
+        duration: 12,
+        ease: "linear",
+        repeatType: "mirror",
+      },
+    },
+  };
+
   return (
     <div className="overflow-y-hidden hidden md:block">
       <motion.div
         className="flex flex-col gap-16 items-center"
-        animate={{ y: isOdd ? ["-100%", "100%"] : ["100%", "-100%"] }}
-        transition={{
-          y: {
-            repeat: Infinity,
-            duration: 12,
-            ease: "linear",
-            repeatType: "mirror",
-          },
-        }}
+        animate={motionProps}
       >
         {projects.map((project) => (
-          <ProjectBox key={project.href} {...project} />
+          <motion.div
+            key={project.href}
+            whileHover={{ y: 0 }}
+            animate={motionProps}
+          >
+            <ProjectBox key={project.href} {...project} />
+          </motion.div>
         ))}
       </motion.div>
     </div>
