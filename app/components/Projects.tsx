efa066a1caf52d5da97babcfd0cb958df2799fa0
gunnmgirl <gunnmgirl@gmail.com>
2024-08-06 13:35:58 +0200

    feat: continue animation position after hover

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { motion } from "framer-motion";
+import { motion, useAnimation, useMotionValue } from "framer-motion";
+import { useEffect } from "react";
 import { ProjectBox } from "./ProjectBox";
 import { ProjectBoxProps } from "../types";
 
@@ -10,22 +11,56 @@ export const Projects = ({
   projects: ProjectBoxProps[];
   isOdd?: boolean;
 }) => {
+  const controls = useAnimation();
+  const y = useMotionValue(isOdd ? -100 : 100);
+
+  useEffect(() => {
+    controls.start({
+      y: isOdd ? ["-100%", "100%"] : ["100%", "-100%"],
+      transition: {
+        y: {
+          repeat: Infinity,
+          duration: projects.length * 3,
+          ease: "linear",
+          repeatType: "loop",
+        },
+      },
+    });
+  }, [isOdd, controls]);
+
+  const handleHoverStart = () => {
+    controls.stop();
+  };
+
+  const handleHoverEnd = () => {
+    controls.start({
+      y: isOdd ? ["-100%", "100%"] : ["100%", "-100%"],
+      transition: {
+        y: {
+          repeat: Infinity,
+          duration: projects.length * 3,
+          ease: "linear",
+          repeatType: "loop",
+          from: y.get(),
+        },
+      },
+    });
+  };
+
   return (
     <div className="overflow-y-hidden hidden md:block">
       <motion.div
         className="flex flex-col gap-16 items-center"
-        animate={{ y: isOdd ? ["-100%", "100%"] : ["100%", "-100%"] }}
-        transition={{
-          y: {
-            repeat: Infinity,
-            duration: 12,
-            ease: "linear",
-            repeatType: "mirror",
-          },
-        }}
+        animate={controls}
       >
         {projects.map((project) => (
-          <ProjectBox key={project.href} {...project} />
+          <motion.div
+            key={project.href}
+            onHoverStart={handleHoverStart}
+            onHoverEnd={handleHoverEnd}
+          >
+            <ProjectBox {...project} />
+          </motion.div>
         ))}
       </motion.div>
     </div>
